84aae0faad03f61b0ee053d9399161b9983f3500
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-05-16 14:35:11 +0700

    feat(task8): Update UI view details for testing data

--- transaction_management.py
+++ transaction_management.py
@@ -1905,13 +1905,133 @@ class ViewDetailsGoldTransactionWindow(customtkinter.CTkToplevel):
             self.after(200, lambda: self.iconbitmap("./logo.ico"))
 
     def create_widget(self):
+        details_frame = customtkinter.CTkFrame(
+            master=self, fg_color="#ffffff", border_width=2,
+            border_color="#5B5B5B")
+        details_frame.pack(padx=20, pady=20, fill="both")
+
         gold_transaction_label = customtkinter.CTkLabel(
-            self, text="Selected Gold Transaction:", font=("Arial", 12, "bold")
+            details_frame, text="GOLD TRANSACTION",
+            font=("Arial", 20, "bold")
         )
-        gold_transaction_label.pack(pady=(10, 5))
+        gold_transaction_label.pack(padx=20, pady=(20, 5), anchor="w")
+
+        gold_transaction_code = customtkinter.CTkLabel(
+            details_frame, text=f"Code: {
+                self.parent.selected_gold_transaction_code}"
+        )
+        gold_transaction_code.pack(padx=20, pady=0, anchor="w")
+
+        separator_style = ttk.Style()
+        separator_style.configure(
+            "Separator.TSeparator", background="#989DA1", borderwidth=1)
+
+        separator = ttk.Separator(
+            details_frame, orient="horizontal", style="Separator.TSeparator")
+        separator.pack(padx=20, pady=(0, 10), fill="x")
+
+        gold_unit_price_frame = customtkinter.CTkFrame(
+            details_frame, fg_color="transparent")
+        gold_unit_price_frame.pack(padx=20, pady=0, fill="x")
+
+        gold_unit_price_label = customtkinter.CTkLabel(
+            gold_unit_price_frame, text="Unit Price (VND/tael):"
+        )
+        gold_unit_price_label.grid(row=0, column=0, padx=0, pady=0,
+                                   sticky="w")
+
+        gold_unit_price_value = customtkinter.CTkLabel(
+            gold_unit_price_frame, font=("Arial", 14, "bold"), text=f"{
+                self.parent.selected_gold_unit_price}"
+        )
+        gold_unit_price_value.grid(row=0, column=1, padx=0, pady=0,
+                                   sticky="e")
+
+        gold_unit_price_frame.columnconfigure(0, weight=0)
+        gold_unit_price_frame.columnconfigure(1, weight=1)
+
+        gold_quantity_frame = customtkinter.CTkFrame(
+            details_frame, fg_color="transparent")
+        gold_quantity_frame.pack(padx=20, pady=0, fill="x")
+
+        gold_quantity_label = customtkinter.CTkLabel(
+            gold_quantity_frame, text="Quantity (tael):"
+        )
+        gold_quantity_label.grid(row=0, column=0, padx=0, pady=0,
+                                 sticky="w")
+
+        gold_quantity_value = customtkinter.CTkLabel(
+            gold_quantity_frame, font=("Arial", 14, "bold"), text=f"{
+                self.parent.selected_gold_quantity}",
+        )
+        gold_quantity_value.grid(row=0, column=1, padx=0, pady=0,
+                                 sticky="e")
+
+        gold_quantity_frame.columnconfigure(0, weight=0)
+        gold_quantity_frame.columnconfigure(1, weight=1)
+
+        gold_type_frame = customtkinter.CTkFrame(
+            details_frame, fg_color="transparent")
+        gold_type_frame.pack(padx=20, pady=0, fill="x")
+
+        gold_type_label = customtkinter.CTkLabel(
+            gold_type_frame, text="Gold Type:"
+        )
+        gold_type_label.grid(row=0, column=0, padx=0, pady=0,
+                             sticky="w")
+
+        gold_type_value = customtkinter.CTkLabel(
+            gold_type_frame, font=("Arial", 14, "bold"), text=f"{
+                self.parent.selected_gold_type}",
+        )
+        gold_type_value.grid(row=0, column=1, padx=0, pady=0,
+                             sticky="e")
+
+        gold_type_frame.columnconfigure(0, weight=0)
+        gold_type_frame.columnconfigure(1, weight=1)
+
+        gold_total_amount_frame = customtkinter.CTkFrame(
+            details_frame, fg_color="transparent")
+        gold_total_amount_frame.pack(padx=20, pady=0, fill="x")
+
+        gold_total_amount_label = customtkinter.CTkLabel(
+            gold_total_amount_frame, text="Total Amount (VND):"
+        )
+        gold_total_amount_label.grid(row=0, column=0, padx=0, pady=0,
+                                     sticky="w")
+
+        gold_total_amount_value = customtkinter.CTkLabel(
+            gold_total_amount_frame, font=("Arial", 14, "bold"), text=f"{
+                self.parent.selected_gold_total_amount}",
+        )
+        gold_total_amount_value.grid(row=0, column=1, padx=0, pady=0,
+                                     sticky="e")
+
+        gold_total_amount_frame.columnconfigure(0, weight=0)
+        gold_total_amount_frame.columnconfigure(1, weight=1)
+
+        gold_transaction_date_frame = customtkinter.CTkFrame(
+            details_frame, fg_color="transparent")
+        gold_transaction_date_frame.pack(padx=20, pady=0, fill="x")
+
+        gold_transaction_date_label = customtkinter.CTkLabel(
+            gold_transaction_date_frame, text="Transaction Date:"
+        )
+        gold_transaction_date_label.grid(row=0, column=0, padx=0, pady=0,
+                                         sticky="w")
+
+        gold_transaction_date_value = customtkinter.CTkLabel(
+            gold_transaction_date_frame, font=("Arial", 14, "bold"), text=f"{
+                1+1}",
+        )
+        gold_transaction_date_value.grid(row=0, column=1, padx=0, pady=0,
+                                         sticky="e")
+
+        gold_transaction_date_frame.columnconfigure(0, weight=0)
+        gold_transaction_date_frame.columnconfigure(1, weight=1)
 
         gold_data_label = customtkinter.CTkLabel(
-            self, text=f"Transaction Code: {
+            details_frame, text=f"Transaction Code: {
                 self.parent.selected_gold_transaction_code}\n"
             f"Unit Price: {self.parent.selected_gold_unit_price}\n"
             f"Quantity: {self.parent.selected_gold_quantity}\n"
