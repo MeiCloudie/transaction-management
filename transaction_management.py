2254259afad6909db8f01d3ae6f57a2644bb4ba1
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-05-24 15:07:04 +0700

    refactor(task13): Add transaction with excel file

--- transaction_management.py
+++ transaction_management.py
@@ -4733,16 +4733,24 @@ class AddTransactionTabView(customtkinter.CTkTabview):
         self.set("GOLD")
         self.configure(corner_radius=5)
 
-        with open('data.json', 'r') as json_file:
-            data = json.load(json_file)
-
-        self.exchange_rates = data['exchange_rates']
+        self.load_exchange_rates_from_excel()
 
         self.create_widgets()
 
+    def load_exchange_rates_from_excel(self):
+        try:
+            df_exchange_rates = pd.read_excel(
+                "data.xlsx", sheet_name="exchange_rates")
+            self.exchange_rates = df_exchange_rates.to_dict('records')
+        except FileNotFoundError:
+            messagebox.showerror("Error", "Data file not found.")
+            self.exchange_rates = []
+        except Exception as e:
+            messagebox.showerror("Error", f"An error occurred: {e}")
+            self.exchange_rates = []
+
     def create_widgets(self):
-        self.create_tab_add_gold_transaction(
-            self.tab_add_gold_transaction)
+        self.create_tab_add_gold_transaction(self.tab_add_gold_transaction)
         self.create_tab_add_currency_transaction(
             self.tab_add_currency_transaction)
 
@@ -4807,8 +4815,7 @@ class AddTransactionTabView(customtkinter.CTkTabview):
 
         separator_day_month = ttk.Separator(
             date_frame, orient="horizontal", style="Separator.TSeparator")
-        separator_day_month.grid(row=0, column=1,
-                                 padx=0, pady=0, sticky="ew")
+        separator_day_month.grid(row=0, column=1, padx=0, pady=0, sticky="ew")
 
         self.gold_entry_month = customtkinter.CTkEntry(
             master=date_frame, placeholder_text="Month", width=95)
@@ -4842,97 +4849,6 @@ class AddTransactionTabView(customtkinter.CTkTabview):
             command=self.master.destroy)
         button_cancel.grid(row=0, column=1, sticky="ew", padx=(6, 0), pady=5)
 
-    def gold_confirm_button_callback(self):
-        unit_price = self.gold_entry_unit_price.get()
-        quantity = self.gold_entry_quantity.get()
-        day_submit = self.gold_entry_day.get()
-        month_submit = self.gold_entry_month.get()
-        year_submit = self.gold_entry_year.get()
-        gold_type = self.gold_combobox_gold_type.get()
-
-        if not all([unit_price, quantity, day_submit, month_submit,
-                    year_submit, gold_type]):
-            messagebox.showerror(
-                "Missing Input", "Please fill in all fields.")
-            self.focus()
-            return
-
-        unit_price = self.validate_and_convert_input(unit_price)
-        if unit_price is None:
-            messagebox.showerror(
-                "Invalid Unit Price", "Unit Price must be a valid number.")
-            self.focus()
-            return
-
-        quantity = self.validate_and_convert_input(quantity)
-        if quantity is None:
-            messagebox.showerror(
-                "Invalid Quantity", "Quantity must be a valid number.")
-            self.focus()
-            return
-
-        if not all(map(str.isdigit, [day_submit, month_submit, year_submit])):
-            messagebox.showerror(
-                "Invalid Input", "Please enter valid numerical \
-                \nvalues for date fields.")
-            self.after(10, self.lift)
-            return
-
-        day = int(day_submit)
-        month = int(month_submit)
-        year = int(year_submit)
-
-        if not self.validate_date(day, month, year):
-            messagebox.showerror("Invalid Date", "Date is not valid.")
-            self.focus()
-            return
-
-        if gold_type not in ["SJC", "PNJ", "DOJI"]:
-            messagebox.showerror("Invalid Gold Type",
-                                 "Gold Type must be 'SJC', 'PNJ', or 'DOJI'.")
-            self.focus()
-            return
-
-        with open("data.json", "r") as file:
-            data = json.load(file)
-
-        new_data = {
-            "id": "",
-            "day": day,
-            "month": month,
-            "year": year,
-            "unit_price": unit_price,
-            "quantity": quantity,
-            "type": "gold",
-            "gold_type": GoldType[self.gold_combobox_gold_type.get()].value,
-            "isdeleted": False
-        }
-
-        new_data_id = self.generate_gold_id(data["transactions"])
-        new_data["id"] = new_data_id
-
-        data["transactions"].append(new_data)
-
-        with open("data.json", "w") as file:
-            json.dump(data, file, indent=4)
-
-        messagebox.showinfo("Success", "Gold transaction added successfully. \
-                            \nPlease Refresh Data!")
-        self.focus()
-
-    def generate_gold_id(self, transactions):
-        gold_transactions = [t for t in transactions if t["type"] == "gold"]
-        num_gold_transactions = len(gold_transactions)
-
-        new_id = f"GLD{num_gold_transactions + 1:03}"
-
-        for transaction in transactions:
-            if transaction["id"] == new_id:
-                num_gold_transactions += 1
-                new_id = f"GLD{num_gold_transactions + 1:03}"
-
-        return new_id
-
     def create_tab_add_currency_transaction(self, tab):
         label_quantity = customtkinter.CTkLabel(
             master=tab,
@@ -4955,13 +4871,9 @@ class AddTransactionTabView(customtkinter.CTkTabview):
         )
         label_currency_type.pack(padx=20, pady=5, anchor="w")
 
-        self.currency_combobox_currency_type = \
-            customtkinter.CTkComboBox(tab,
-                                      values=[
-                                          "VND", "USD", "EUR"
-                                      ],
-                                      command=self.
-                                      combobox_currency_type_callback)
+        self.currency_combobox_currency_type = customtkinter.CTkComboBox(
+            tab, values=["VND", "USD", "EUR"],
+            command=self.combobox_currency_type_callback)
         self.currency_combobox_currency_type.set("VND")
         self.currency_combobox_currency_type.pack(
             padx=20, pady=0, anchor="w", fill="x")
@@ -4976,8 +4888,8 @@ class AddTransactionTabView(customtkinter.CTkTabview):
 
         self.currency_entry_exchange_rate = customtkinter.CTkEntry(
             master=tab, placeholder_text="23,130.6")
-        self.currency_entry_exchange_rate.pack(padx=20, pady=0, anchor="w",
-                                               fill="x")
+        self.currency_entry_exchange_rate.pack(padx=20, pady=0,
+                                               anchor="w", fill="x")
         self.currency_entry_exchange_rate.insert(
             0, str(self.exchange_rates[0]["rate"]))
         self.currency_entry_exchange_rate.configure(state="readonly")
@@ -4998,22 +4910,20 @@ class AddTransactionTabView(customtkinter.CTkTabview):
 
         self.currency_entry_day = customtkinter.CTkEntry(
             master=date_frame, placeholder_text="Day", width=95)
-        self.currency_entry_day.grid(row=0, column=0, padx=(0, 5), pady=0,
-                                     sticky="ew")
+        self.currency_entry_day.grid(row=0, column=0, padx=(0, 5),
+                                     pady=0, sticky="ew")
 
-        separator_day_month = ttk.Separator(
-            date_frame, orient="horizontal", style="Separator.TSeparator")
-        separator_day_month.grid(row=0, column=1,
-                                 padx=0, pady=0, sticky="ew")
+        separator_day_month = ttk.Separator(date_frame, orient="horizontal",
+                                            style="Separator.TSeparator")
+        separator_day_month.grid(row=0, column=1, padx=0, pady=0, sticky="ew")
 
         self.currency_entry_month = customtkinter.CTkEntry(
             master=date_frame, placeholder_text="Month", width=95)
         self.currency_entry_month.grid(row=0, column=2, padx=5, pady=0)
 
-        separator_month_year = ttk.Separator(
-            date_frame, orient="horizontal", style="Separator.TSeparator")
-        separator_month_year.grid(
-            row=0, column=3, padx=0, pady=0, sticky="ew")
+        separator_month_year = ttk.Separator(date_frame, orient="horizontal",
+                                             style="Separator.TSeparator")
+        separator_month_year.grid(row=0, column=3, padx=0, pady=0, sticky="ew")
 
         self.currency_entry_year = customtkinter.CTkEntry(
             master=date_frame, placeholder_text="Year", width=95)
@@ -5038,6 +4948,90 @@ class AddTransactionTabView(customtkinter.CTkTabview):
             command=self.master.destroy)
         button_cancel.grid(row=0, column=1, sticky="ew", padx=(6, 0), pady=5)
 
+    def gold_confirm_button_callback(self):
+        unit_price = self.gold_entry_unit_price.get()
+        quantity = self.gold_entry_quantity.get()
+        day_submit = self.gold_entry_day.get()
+        month_submit = self.gold_entry_month.get()
+        year_submit = self.gold_entry_year.get()
+        gold_type = self.gold_combobox_gold_type.get()
+
+        if not all([unit_price, quantity, day_submit, month_submit,
+                    year_submit, gold_type]):
+            messagebox.showerror("Missing Input", "Please fill in all fields.")
+            self.focus()
+            return
+
+        unit_price = self.validate_and_convert_input(unit_price)
+        if unit_price is None:
+            messagebox.showerror("Invalid Unit Price",
+                                 "Unit Price must be a valid number.")
+            self.focus()
+            return
+
+        quantity = self.validate_and_convert_input(quantity)
+        if quantity is None:
+            messagebox.showerror("Invalid Quantity",
+                                 "Quantity must be a valid number.")
+            self.focus()
+            return
+
+        if not all(map(str.isdigit, [day_submit, month_submit, year_submit])):
+            messagebox.showerror(
+                "Invalid Input",
+                "Please enter valid numerical values for date fields.")
+            self.after(10, self.lift)
+            return
+
+        day = int(day_submit)
+        month = int(month_submit)
+        year = int(year_submit)
+
+        if not self.validate_date(day, month, year):
+            messagebox.showerror("Invalid Date", "Date is not valid.")
+            self.focus()
+            return
+
+        if gold_type not in ["SJC", "PNJ", "DOJI"]:
+            messagebox.showerror("Invalid Gold Type",
+                                 "Gold Type must be 'SJC', 'PNJ', or 'DOJI'.")
+            self.focus()
+            return
+
+        try:
+            new_data = {
+                "id": self.generate_gold_id(
+                    pd.read_excel("data.xlsx", sheet_name="transactions")),
+                "day": day,
+                "month": month,
+                "year": year,
+                "unit_price": unit_price,
+                "quantity": quantity,
+                "type": "gold",
+                "gold_type": GoldType[gold_type].value,
+                "isdeleted": False
+            }
+            self.append_to_excel(new_data, "transactions")
+
+            messagebox.showinfo(
+                "Success",
+                "Gold transaction added successfully. Please Refresh Data!")
+            self.focus()
+        except Exception as e:
+            messagebox.showerror(
+                "Error",
+                f"An error occurred while writing to Excel: {e}")
+            self.focus()
+
+    def generate_gold_id(self, df_transactions):
+        gold_transactions = df_transactions[df_transactions["type"] == "gold"]
+        num_gold_transactions = len(gold_transactions)
+        new_id = f"GLD{num_gold_transactions + 1:03}"
+        while new_id in df_transactions["id"].values:
+            num_gold_transactions += 1
+            new_id = f"GLD{num_gold_transactions + 1:03}"
+        return new_id
+
     def combobox_currency_type_callback(self, choice):
         try:
             currency_type = CurrencyType[choice].value
@@ -5059,38 +5053,28 @@ class AddTransactionTabView(customtkinter.CTkTabview):
 
     def currency_confirm_button_callback(self):
         quantity = self.currency_entry_quantity.get()
-        exchange_rate = self.currency_entry_exchange_rate.get()
         day_submit = self.currency_entry_day.get()
         month_submit = self.currency_entry_month.get()
         year_submit = self.currency_entry_year.get()
         currency_type = self.currency_combobox_currency_type.get()
 
-        if not all([quantity, exchange_rate, day_submit, month_submit,
+        if not all([quantity, day_submit, month_submit,
                     year_submit, currency_type]):
-            messagebox.showerror(
-                "Missing Input", "Please fill in all fields.")
+            messagebox.showerror("Missing Input", "Please fill in all fields.")
             self.focus()
             return
 
         quantity = self.validate_and_convert_input(quantity)
         if quantity is None:
-            messagebox.showerror(
-                "Invalid Quantity", "Quantity must be a valid number.")
-            self.focus()
-            return
-
-        exchange_rate = self.validate_and_convert_input(exchange_rate)
-        if exchange_rate is None:
-            messagebox.showerror(
-                "Invalid Exchange Rate",
-                "Exchange Rate must be a valid number.")
+            messagebox.showerror("Invalid Quantity",
+                                 "Quantity must be a valid number.")
             self.focus()
             return
 
         if not all(map(str.isdigit, [day_submit, month_submit, year_submit])):
             messagebox.showerror(
-                "Invalid Input", "Please enter valid numerical \
-                \nvalues for date fields.")
+                "Invalid Input",
+                "Please enter valid numerical values for date fields.")
             self.after(10, self.lift)
             return
 
@@ -5104,68 +5088,94 @@ class AddTransactionTabView(customtkinter.CTkTabview):
             return
 
         if currency_type not in ["VND", "USD", "EUR"]:
-            messagebox. \
-                showerror("Invalid Currency Type",
-                          "Currency Type must be 'VND', 'USD', or 'EUR'.")
+            messagebox.showerror(
+                "Invalid Currency Type",
+                "Currency Type must be 'VND', 'USD', or 'EUR'.")
             self.focus()
             return
 
-        with open("data.json", "r") as file:
-            data = json.load(file)
+        try:
+            df_transactions = pd.read_excel(
+                "data.xlsx", sheet_name="transactions")
+            df_exchange_rates = pd.read_excel(
+                "data.xlsx", sheet_name="exchange_rates")
 
-        exchange_rates = data["exchange_rates"]
+            currency_type_enum = CurrencyType[currency_type].value
+            exchange_rate_row = df_exchange_rates[
+                df_exchange_rates["currency_type"] ==
+                currency_type_enum].iloc[0]
+            exchange_rate = exchange_rate_row["rate"]
+            exchange_rate_id = exchange_rate_row["id"]
 
-        exchange_rate = None
-        for rate in exchange_rates:
-            if rate["currency_type"] == CurrencyType[currency_type].value:
-                exchange_rate = rate
-                break
+            new_data = {
+                "id": self.generate_currency_id(df_transactions),
+                "day": day,
+                "month": month,
+                "year": year,
+                "quantity": quantity,
+                "type": "currency",
+                "currency_type": currency_type_enum,
+                "exchange_rate_id": exchange_rate_id,
+                "exchange_rate": exchange_rate,
+                "effective_day": exchange_rate_row["effective_day"],
+                "effective_month": exchange_rate_row["effective_month"],
+                "effective_year": exchange_rate_row["effective_year"],
+                "isdeleted": False
+            }
 
-        if exchange_rate is None:
-            messagebox.showerror("Exchange Rate Not Found",
-                                 f"No exchange rate found for currency type \
-                                {currency_type}.")
+            self.append_to_excel(new_data, "transactions")
+
+            messagebox.showinfo(
+                "Success",
+                "Currency transaction added successfully. Please Refresh Data!"
+            )
             self.focus()
-            return
+        except Exception as e:
+            messagebox.showerror(
+                "Error", f"An error occurred while writing to Excel: {e}")
+            self.focus()
 
-        new_data = {
-            "id": "",
-            "day": day,
-            "month": month,
-            "year": year,
-            "quantity": quantity,
-            "type": "currency",
-            "currency_type": CurrencyType[currency_type].value,
-            "exchange_rate": exchange_rate,
-            "isdeleted": False
-        }
-
-        new_data_id = self.generate_currency_id(data["transactions"])
-        new_data["id"] = new_data_id
-
-        data["transactions"].append(new_data)
-
-        with open("data.json", "w") as file:
-            json.dump(data, file, indent=4)
-
-        messagebox.showinfo("Success",
-                            "Currency transaction added successfully. \
-                            Please Refresh Data!")
-        self.focus()
-
-    def generate_currency_id(self, transactions):
-        currency_transactions = [
-            t for t in transactions if t["type"] == "currency"]
+    def generate_currency_id(self, df_transactions):
+        currency_transactions = df_transactions[
+            df_transactions["type"] == "currency"]
         num_currency_transactions = len(currency_transactions)
-
         new_id = f"CUR{num_currency_transactions + 1:03}"
-
-        for transaction in transactions:
-            if transaction["id"] == new_id:
-                num_currency_transactions += 1
-                new_id = f"CUR{num_currency_transactions + 1:03}"
-
+        while new_id in df_transactions["id"].values:
+            num_currency_transactions += 1
+            new_id = f"CUR{num_currency_transactions + 1:03}"
         return new_id
+
+    def append_to_excel(self, new_data, sheet_name):
+        try:
+            df_transactions = pd.read_excel(
+                "data.xlsx", sheet_name="transactions")
+            df_exchange_rates = pd.read_excel(
+                "data.xlsx", sheet_name="exchange_rates")
+
+            if sheet_name == "transactions":
+                df_transactions = pd.concat(
+                    [df_transactions, pd.DataFrame([new_data])],
+                    ignore_index=True)
+            elif sheet_name == "exchange_rates":
+                df_exchange_rates = pd.concat(
+                    [df_exchange_rates, pd.DataFrame([new_data])],
+                    ignore_index=True)
+
+            with pd.ExcelWriter("data.xlsx", engine="openpyxl", mode="a",
+                                if_sheet_exists="replace") as writer:
+                df_transactions.to_excel(
+                    writer, sheet_name="transactions", index=False)
+                df_exchange_rates.to_excel(
+                    writer, sheet_name="exchange_rates", index=False)
+        except Exception as e:
+            messagebox.showerror(
+                "Error", f"An error occurred while writing to Excel: {e}")
+
+    def get_exchange_rate_id(self, currency_type):
+        for rate in self.exchange_rates:
+            if rate["currency_type"] == CurrencyType[currency_type].value:
+                return rate["id"]
+        return None
 
     def validate_and_convert_input(self, input_str):
         try:
